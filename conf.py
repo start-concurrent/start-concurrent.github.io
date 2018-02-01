178e355bf84bfd3d97ca319b3a6965931baca1f2
Tim Korb <jtkorb@bikmort.com>
2018-02-01 16:24:10 -0500

    Switched to Sphinx alabaster theme.

--- conf.py
+++ conf.py
@@ -54,8 +54,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
 html_theme = 'classic'
+html_theme = 'alabaster'
 
 if html_theme == 'alabaster':
     html_theme_options = {
