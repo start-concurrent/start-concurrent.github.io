ad00e6f3c74120da11b837e173d560beec8820a6
Tim Korb <jtkorb@bikmort.com>
2018-06-08 09:41:05 -0400

    Added missing programs to Arrays chapter.

---
+++ chapters/06-arrays/programs/QuietSummer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class QuietSummer {
+	public static void main(String[] args) {
+		Scanner in = new Scanner( System.in );
+		int sum = 0;
+		while( in.hasNextInt() )
+			sum += in.nextInt();		
+		System.out.println("The sum of the numbers is " + sum);
+	}
+}
