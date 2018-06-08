e8b9da27ac1a23df5db218c57b4075da8e0d147a
Tim Korb <jtkorb@bikmort.com>
2018-06-08 12:14:49 -0400

    Added programs that hadn't been included before.

---
+++ chapters/11-inheritance/programs/GraduateStudent.java
@@ -0,0 +1,15 @@
+public class GraduateStudent extends Student {
+	private String topic;
+
+	public GraduateStudent(String name, double GPA, String topic ) {
+		super( name, 4, GPA );
+		setTopic( topic );
+	}	
+
+	public void setTopic(String topic) { this.topic = topic; }	
+
+	public String toString() {
+		return getName() + "\t" + "Graduate" +
+			"\t" + getGPA() + "\tTopic: " + topic;
+	}	
+}
