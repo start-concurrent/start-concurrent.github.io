f753587a8145fd089c7848fa4e947abebf14ad26
Tim Korb <jtkorb@bikmort.com>
2018-06-09 11:13:17 -0400

    Added code and files for partListing macro in LaTeX source.

---
+++ chapters/19-recursion/programs/MazeSolver.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class MazeSolver {
+	private char[][] maze;
+	private int rows, columns;
+
+	public static void main( String[] args ) {
+		MazeSolver solver = new MazeSolver();
+		if( solver.solve(0, 0) )/*@\label{mazeRCall}@*/
+			System.out.println("\nSolved!");
+		else
+			System.out.println("\nNot solvable!");
+		solver.print();/*@\label{mazeRPrint}@*/
+	}	
+
+	public MazeSolver() {
+		Scanner in = new Scanner(System.in);
+		rows = in.nextInt();
+		columns = in.nextInt();
+		in.nextLine();
+		maze = new char[rows][columns];
+		for( int row = 0; row < rows; row++ ) {
+			String line = in.nextLine();
+			System.out.println(line);
+			for (int column = 0; column < columns; column++) {
+				maze[row][column] = line.charAt(column);
+			}
+		}
+	}
+
+	public void print() {
+		for( int row = 0; row < rows; row++ ) {
+			for (int column = 0; column < columns; column++)
+				System.out.print(maze[row][column]);
+			System.out.println();
+		}
+	}
+
+	public boolean solve( int row, int column ) {/*@\label{mazeRBegin}@*/
+		if( row < 0 || column < 0 || row >= rows || column >= columns)/*@\label{mazeROutside}@*/
+			return false;
+		else if( maze[row][column] == 'E' )/*@\label{mazeRDone}@*/
+			return true;
+		else if( maze[row][column] != ' ')/*@\label{mazeRBlocked}@*/
+			return false;
+		else {
+			maze[row][column] = '*';
+			if( solve(row - 1, column) || solve(row + 1, column) ||/*@\label{mazeRRecursive}@*/
+				solve(row, column - 1) || solve(row, column + 1) )
+				return true;
+			else {
+				maze[row][column] = ' ';/*@\label{mazeRBlank}@*/
+				return false;
+			}
+		}
+	}
+}
