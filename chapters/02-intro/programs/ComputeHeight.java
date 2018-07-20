239b8b70c56f8b83997531f7f9c9cbc7ebb1c595
Barry <bwittman@gmail.com>
2018-07-20 12:13:55 -0400

    Updated Chapter 2 to use program callouts

--- chapters/02-intro/programs/ComputeHeight.java
+++ chapters/02-intro/programs/ComputeHeight.java
@@ -4,7 +4,7 @@ public class ComputeHeight {
         double height = 15, coefficient = 0.3;
         int bounces = 10;
         // Compute height after bounces
-        double bounceHeight = height*Math.pow(coefficient,bounces);/*@\label{computeHeight}@*/
+        double bounceHeight = height*Math.pow(coefficient,bounces);
         System.out.println(bounceHeight); // For testing
     }
 }
