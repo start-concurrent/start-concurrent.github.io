f42787623b40fb6730e606fa2fbdfaa38164611c
Barry <bwittman@gmail.com>
2018-07-20 14:18:33 -0400

    Updated Chapter 3 to use program callouts

--- chapters/03-strings-primitive-types/programs/DragonAttributes.java
+++ chapters/03-strings-primitive-types/programs/DragonAttributes.java
@@ -1,13 +1,13 @@
-import java.util.*;
+import java.util.*; // <1>
 
 public class DragonAttributes {    
     public static void main(String[] args) {
-    	Random random = new Random();/*@\label{createRandomNumberGeneratorLine}@*/
-        int age = random.nextInt(100) + 1;/*@\label{generateAgeLine}@*/
-        double height = random.nextDouble()*30;/*@\label{generateHeightLine}@*/
-        boolean gender = random.nextBoolean();/*@\label{generateStatusLine}@*/
-        int hitPoints = random.nextInt(51) + 25;
-        System.out.println("Dragon Statistics");/*@\label{displayTurtleAttributesLine}@*/
+    	Random random = new Random(); // <2>
+        int age = random.nextInt(100) + 1; // <3>
+        double height = random.nextDouble()*30; // <4>
+        boolean gender = random.nextBoolean(); // <5>
+        int hitPoints = random.nextInt(51) + 25; // <6>
+        System.out.println("Dragon Statistics");
         System.out.println("Age:\t\t" + age);
         System.out.format("Height:\t\t%.1f feet\n", height);
         System.out.println("Female:\t\t" + gender);
