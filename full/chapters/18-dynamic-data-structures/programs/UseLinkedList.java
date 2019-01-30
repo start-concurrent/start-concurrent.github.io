506d6fdda4dff37d9a4d64023944e21b0c5a88b5
Barry <bwittman@gmail.com>
2019-01-30 11:07:55 -0500

    Removed remaining old-style line number references
    Either changed them to new-style callouts or removed them entirely.

--- full/chapters/18-dynamic-data-structures/programs/UseLinkedList.java
+++ full/chapters/18-dynamic-data-structures/programs/UseLinkedList.java
@@ -7,12 +7,12 @@ public class UseLinkedList {
         LinkedList list = new LinkedList();
 
         while (in.hasNextLine())
-            list.add(in.nextLine());/*@\label{lineReadNew}@*/
+            list.add(in.nextLine());
         
         String[] names = new String[list.getSize()]; 
         list.fillArray(names);
 
-        Arrays.sort(names);/*@\label{lineSort}@*/
+        Arrays.sort(names);
         
         for (int i = 0; i < names.length; i++)
             System.out.println(names[i]);
