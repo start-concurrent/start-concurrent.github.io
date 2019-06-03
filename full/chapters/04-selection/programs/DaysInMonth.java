4332f2fdf61464d6240d2d4cb72af535716d262d
Barry <bwittman@gmail.com>
2019-06-03 15:14:04 -0400

    Updated through Chapter 4
    Various copy-editing and formatting improvements.
    
    Note that this change also changes the setting back to latexmath instead of asciimath.

--- full/chapters/04-selection/programs/DaysInMonth.java
+++ full/chapters/04-selection/programs/DaysInMonth.java
@@ -7,12 +7,12 @@ public class DaysInMonth {
         int month = in.nextInt();
         int days = 0;
         switch( month ) {
-            case 2:  days = 28; break;
+            case 2:  days = 28; break; //<1>
 
             case 4:
             case 6:
             case 9:
-            case 11: days = 30; break;
+            case 11: days = 30; break; //<2>
 
             case 1:
             case 3:
@@ -20,7 +20,7 @@ public class DaysInMonth {
             case 7:
             case 8:
             case 10:
-            case 12: days = 31; break;
+            case 12: days = 31; break; //<3>
         }   
         System.out.println("The month you entered has " +
             days + " days.");
