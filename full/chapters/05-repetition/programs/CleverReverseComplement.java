7c9bf074f008df1fd441cb6db131536bd602d4d4
Barry <bwittman@gmail.com>
2019-06-04 14:36:43 -0400

    Updated Chapter 5
    Mostly copy-editing, formatting, a few bug fixes in programs.

--- full/chapters/05-repetition/programs/CleverReverseComplement.java
+++ full/chapters/05-repetition/programs/CleverReverseComplement.java
@@ -8,16 +8,11 @@ public class CleverReverseComplement {
         String reverseComplement = "";
         for( int i = 0; i < sequence.length(); i++ )
             switch( sequence.charAt(i) ) { //get complements
-                case 'A': reverseComplement = "T" +
-                    reverseComplement; break;
-                case 'C': reverseComplement = "G" +
-                    reverseComplement; break;
-                case 'G': reverseComplement = "C" +
-                    reverseComplement; break;
-                case 'T': reverseComplement = "A" +
-                    reverseComplement; break;
+                case 'A': reverseComplement = "T" + reverseComplement; break;
+                case 'C': reverseComplement = "G" + reverseComplement; break;
+                case 'G': reverseComplement = "C" + reverseComplement; break;
+                case 'T': reverseComplement = "A" + reverseComplement; break;
             }               
-        System.out.println("Reverse complement: " +
-            reverseComplement);
+        System.out.println("Reverse complement: " + reverseComplement);
     }
 }
--- full/chapters/05-repetition/programs/ReverseComplement.java
+++ full/chapters/05-repetition/programs/ReverseComplement.java
@@ -6,7 +6,7 @@ public class ReverseComplement {
         System.out.print("Please enter a DNA sequence: ");
         String sequence = in.next().toUpperCase();              
         String complement = "";
-        for( int i = 0; i < sequence.length(); i++ )
+        for( int i = 0; i < sequence.length(); i++ ) 		//<1>
             switch( sequence.charAt(i) ) { //get complements
                 case 'A': complement += "T"; break;
                 case 'C': complement += "G"; break;
@@ -15,9 +15,8 @@ public class ReverseComplement {
             }       
         String reverseComplement = "";
         //reverse the complement
-        for( int i = complement.length() - 1; i >= 0; i-- ) 
+        for( int i = complement.length() - 1; i >= 0; i-- )	//<2> 
             reverseComplement += complement.charAt(i);
-        System.out.println("Reverse complement: " +
-            reverseComplement);
+        System.out.println("Reverse complement: " + reverseComplement);
     }
 }
