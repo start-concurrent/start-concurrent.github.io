4d3a9d0fecf00dfea1af31e63e1bbeba066efbb1
Barry <bwittman@gmail.com>
2019-06-07 11:44:40 -0400

    Added callouts to programs included in several segments
    Until recently, it was very difficult to add callouts to a program that was included in several segments.
    
    Numbering the callouts inside the program sequentially doesn't work.  If a program has callouts <1> and <2> in its first segment and <3> and <4> in the second, Asciidoctor complains when the <3> and <4> are listed to describe the second section, since each block of callout numbers are supposed to start at <1>.
    
    The solution adopted by this commit is to use callout autonumbering (using <.> for all callouts).  The benefits of callout autonumbering are that they don't' require the author to keep track of specific numbers and each segment of code automatically starts at <1>.  This solution works even if included code segments overlap.
    
    In my opinion, however, it would be nice if callout numbering could start at particular points, to show progression through a program.  After a comment thread with Dan Allen, I intend to make an issue request for this feature in Asciidoctor.

--- full/chapters/03-strings-primitive-types/programs/CollegeCosts.java
+++ full/chapters/03-strings-primitive-types/programs/CollegeCosts.java
@@ -1,35 +1,35 @@
-import java.util.*;
+import java.util.*; //<.>
 
 public class CollegeCosts {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the College Cost Calculator!");
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) { //<.>
+        System.out.println("Welcome to the College Cost Calculator!"); //<.>
+        Scanner in = new Scanner(System.in); //<.>
         
         System.out.print("Enter your first name:\t\t");
-        String firstName = in.next();
+        String firstName = in.next(); //<.>
         System.out.print("Enter your last name:\t\t");
-        String lastName = in.next();
+        String lastName = in.next(); //<.>
         System.out.print("Enter tuition per semester:\t$");
-        double semesterTuition = in.nextDouble();
+        double semesterTuition = in.nextDouble(); //<.>
         System.out.print("Enter rent per month:\t\t$");
-        double monthlyRent = in.nextDouble();
+        double monthlyRent = in.nextDouble(); //<.>
         System.out.print("Enter food cost per month:\t$");
-        double monthlyFood = in.nextDouble();
+        double monthlyFood = in.nextDouble(); //<.>
         System.out.print("Annual interest rate:\t\t");
-        double annualInterest = in.nextDouble();        
+        double annualInterest = in.nextDouble(); //<.>       
         System.out.print("Years to pay back your loan:\t");
-        int years = in.nextInt();       
+        int years = in.nextInt(); //<.>
         
-        double yearlyCost = semesterTuition * 2.0 + (monthlyRent + monthlyFood) * 12.0;
-        double fourYearCost = yearlyCost * 4.0;
-        double monthlyInterest = annualInterest / 12.0;
-        double monthlyPayment = fourYearCost * monthlyInterest /
+        double yearlyCost = semesterTuition * 2.0 + (monthlyRent + monthlyFood) * 12.0; //<.>
+        double fourYearCost = yearlyCost * 4.0; //<.>
+        double monthlyInterest = annualInterest / 12.0; //<.>
+        double monthlyPayment = fourYearCost * monthlyInterest / //<.>
             (1.0 - Math.pow(1.0 + monthlyInterest, -years * 12.0));
-        double totalLoanCost = monthlyPayment * 12.0 * years;
+        double totalLoanCost = monthlyPayment * 12.0 * years; //<.>
                 
-        System.out.println("\nCollege costs for " + firstName + " " + lastName );
+        System.out.println("\nCollege costs for " + firstName + " " + lastName ); //<.>
         System.out.println("***************************************");
-        System.out.format("Yearly cost:\t\t\t$%.2f%n", yearlyCost);
+        System.out.format("Yearly cost:\t\t\t$%.2f%n", yearlyCost); //<.>
         System.out.format("Four year cost:\t\t\t$%.2f%n", fourYearCost);
         System.out.format("Monthly loan payment:\t\t$%.2f%n", monthlyPayment);
         System.out.format("Total loan cost:\t\t$%.2f%n", totalLoanCost );
--- full/chapters/05-repetition/programs/DNASearch.java
+++ full/chapters/05-repetition/programs/DNASearch.java
@@ -2,33 +2,31 @@ import java.util.*;
 
 public class DNASearch {
     public static void main(String[] args) {                
-        Scanner in = new Scanner( System.in );  
+        Scanner in = new Scanner( System.in ); //<.>
         String sequence, subsequence;
-        boolean valid;
+        boolean valid; //<.>
         char c;
         
         do {
-            System.out.print(
-                "Enter the DNA sequence you wish to search in: ");
-            sequence = in.next().toUpperCase();
+            System.out.print("Enter the DNA sequence you wish to search in: "); //<.>
+            sequence = in.next().toUpperCase(); //<.>
             valid = true;
-            for(int i = 0; i < sequence.length() && valid; i++) {               
+            for(int i = 0; i < sequence.length() && valid; i++) { //<.>              
                 c = sequence.charAt(i);
-                if( c != 'A' && c != 'C' && c != 'G' && c != 'T') {
+                if(c != 'A' && c != 'C' && c != 'G' && c != 'T') { //<.>
                     System.out.println("Invalid DNA sequence!");
                     valid = false;
                 }
             }
-        } while( !valid );
+        } while( !valid ); //<.>
         
         do {        
-            System.out.print(
-                "Enter the subsequence you wish to search for: ");
+            System.out.print("Enter the subsequence you wish to search for: ");
             subsequence  = in.next().toUpperCase();
             valid = true;
             for(int i = 0; i < subsequence.length() && valid; i++) {                
                 c = subsequence.charAt(i);
-                if( c != 'A' && c != 'C' && c != 'G' && c != 'T') {
+                if(c != 'A' && c != 'C' && c != 'G' && c != 'T') {
                     System.out.println("Invalid DNA sequence!");
                     valid = false;
                 }
@@ -36,19 +34,18 @@ public class DNASearch {
         } while( !valid );
         
         int found = 0;
-        for( int i = 0; i < sequence.length() -
-            subsequence.length() + 1; i++ ) {           
-            for( int j = 0; j < subsequence.length(); j++ ) {
-                if(subsequence.charAt(j) != sequence.charAt(i + j))
+        for(int i = 0; i < sequence.length() - subsequence.length() + 1; i++) { //<.>
+            for(int j = 0; j < subsequence.length(); j++) { //<.>
+                if(subsequence.charAt(j) != sequence.charAt(i + j)) //<.>
                     break;
-                if( j == subsequence.length() - 1 ) { //matches
+                if(j == subsequence.length() - 1) { //matches <.>
                     System.out.println("Match found at index " + i);
                     found++;
                 }
             }
         }
         
-        if( found == 1 )
+        if(found == 1)
             System.out.println("One match found.");
         else
             System.out.println(found + " matches found.");
