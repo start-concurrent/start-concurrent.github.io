72c227d01f1b5241d7e25b214d8c7a51860cf066
Barry <bwittman@gmail.com>
2019-06-26 11:26:05 -0400

    Updated Chapter 8
    Copy editing and removal of all stem (LaTeX).

--- full/chapters/08-methods/programs/AlwaysEven.java
+++ full/chapters/08-methods/programs/AlwaysEven.java
@@ -2,11 +2,11 @@ public class AlwaysEven {
     private static int value = 1;
 
     public static void print() {        
-        value++;
-        if( value % 2 == 0 )
+        value++; //<.>
+        if(value % 2 == 0)
             System.out.println("Even");
         else
             System.out.println("Odd");
-        value++;
+        value++; //<.>
     }
 }
