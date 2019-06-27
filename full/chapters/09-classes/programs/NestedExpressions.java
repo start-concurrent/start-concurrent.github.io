374ee5e70d588a0ab656f61f61edb07e444e2334
Barry <bwittman@gmail.com>
2019-06-27 18:32:59 -0400

    Updated Chapter 9

--- full/chapters/09-classes/programs/NestedExpressions.java
+++ full/chapters/09-classes/programs/NestedExpressions.java
@@ -2,44 +2,34 @@ import java.util.*;
 
 public class NestedExpressions {
     public static void main(String[] args) {        
-        Scanner in = new Scanner( System.in );
-        String input = in.nextLine();
-        SymbolStack stack = new SymbolStack(input.length());                
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine(); //<.>
+        SymbolStack stack = new SymbolStack(input.length()); //<.>
         char symbol;    
-        boolean correct = true;
-        for( int i = 0; i < input.length() && correct; i++ ) {
+        boolean correct = true; //<.>
+        for(int i = 0; i < input.length() && correct; i++) { //<.>
             symbol = input.charAt(i);
-            switch( symbol ) {
+            switch(symbol) {
                 case '(':
                 case '[':
                 case '{':
-                    stack.push(symbol);
+                    stack.push(symbol); //<.>
                     break;
                 case ')':
-                    if(stack.isEmpty() || stack.top() != '(')
+				case ']':
+				case '}':
+                    if(stack.isEmpty() || stack.top() != symbol) //<.>
                         correct = false;
                     else
                         stack.pop();
-                    break;
-                case ']':
-                    if(stack.isEmpty() || stack.top() != '[')
-                        correct = false;
-                    else
-                        stack.pop();
-                    break;
-                case '}':
-                    if(stack.isEmpty() || stack.top() != '{')
-                        correct = false;
-                    else
-                        stack.pop();
-                    break;
+                    break;                
             }
         }
             
-        if( !stack.isEmpty() ) //unmatched left symbols
+        if(!stack.isEmpty()) //unmatched left symbols <.>
             correct = false;
         
-        if( correct )
+        if(correct) //<.>
             System.out.println("The input is correctly nested!");
         else
             System.out.println("The input is incorrectly nested!");
