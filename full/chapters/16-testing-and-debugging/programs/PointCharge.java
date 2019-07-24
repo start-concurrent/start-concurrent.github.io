2a1de4da46b63ef87b3ebbf64b3406a9797c93ff
Barry <bwittman@gmail.com>
2019-07-24 16:22:20 -0400

    Finished updating Chapter 16
    . Migrated JUnit code to JUnit 5
    . Updated concurrent testing tools to modern equivalents (which are not great)
    . Copy editing

--- full/chapters/16-testing-and-debugging/programs/PointCharge.java
+++ full/chapters/16-testing-and-debugging/programs/PointCharge.java
@@ -5,7 +5,7 @@ public class PointCharge {
 	private double z;		// m
     public static final double K = 8.9875517873681764e9; // N m^2 C^-2  
       
-    public PointCharge(double charge, double x, double y, double z) {
+    public PointCharge(double charge, double x, double y, double z) { //<.>
         this.charge = charge;    
         this.x = x;
         this.y = y;
@@ -14,23 +14,23 @@ public class PointCharge {
   
     public double getCharge() { return charge; }
   
-    public double distance(PointCharge p) {
+    public double distance(PointCharge p) { //<.>
         return distance(p.x, p.y, p.z);
     } 
   
-    private double distance(double x, double y, double z) {
+    private double distance(double x, double y, double z) { //<.>
         double deltaX = this.x - x;
         double deltaY = this.y - y;
         double deltaZ = this.z - z;    
         return Math.sqrt(deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ);      
     }
   
-    public double scalarForce(PointCharge p) {
+    public double scalarForce(PointCharge p) { //<.>
         double r = distance(p);
         return K*charge*p.charge/r*r;
     }
   
-    public double fieldMagnitude(double x, double y, double z) {
+    public double fieldMagnitude(double x, double y, double z) { //<.>
         double r = distance(x, y, z);
         return charge/(r*r);
     }
--- full/chapters/16-testing-and-debugging/programs/TestPointCharge.java
+++ full/chapters/16-testing-and-debugging/programs/TestPointCharge.java
@@ -17,24 +17,24 @@ public class TestPointCharge {
 
 	@Test
 	public void distanceTest() {
-		assertEquals(1.0, charge1.distance(charge2), 0.001);
-		assertEquals(1.0, charge1.distance(charge3), 0.001);
-		assertEquals(1.0, charge1.distance(charge4), 0.001);
-		assertEquals(Math.sqrt(2.0), charge2.distance(charge3), 0.001);
+		assertEquals(1.0, charge1.distance(charge2), 0.000001);
+		assertEquals(1.0, charge1.distance(charge3), 0.000001);
+		assertEquals(1.0, charge1.distance(charge4), 0.000001);
+		assertEquals(Math.sqrt(2.0), charge2.distance(charge3), 0.000001);
 	} 
 
 	@Test
 	public void scalarForceTest() {
-		assertEquals(2*PointCharge.K, charge1.scalarForce(charge2), 0.001);
-		assertEquals(-PointCharge.K, charge1.scalarForce(charge3), 0.001);
-		assertEquals(0.0, charge1.scalarForce(charge4), 0.001);
-		assertEquals((double)-PointCharge.K, (double)charge2.scalarForce(charge3), 0.001);
+		assertEquals(2*PointCharge.K, charge1.scalarForce(charge2), 0.000001);
+		assertEquals(-PointCharge.K, charge1.scalarForce(charge3), 0.000001);
+		assertEquals(0.0, charge1.scalarForce(charge4), 0.000001);
+		assertEquals((double)-PointCharge.K, (double)charge2.scalarForce(charge3), 0.000001);
 	}   
 
 	@Test
 	public void fieldMagnitudeTest() {
-		assertEquals(2*PointCharge.K, charge2.fieldMagnitude(0, 0, 0), 0.001);
-		assertEquals(PointCharge.K, charge2.fieldMagnitude(0, 1, 0), 0.001);
-		assertEquals(PointCharge.K, charge2.fieldMagnitude(0, 0, 1), 0.001);    
+		assertEquals(2*PointCharge.K, charge2.fieldMagnitude(0, 0, 0), 0.000001);
+		assertEquals(PointCharge.K, charge2.fieldMagnitude(0, 1, 0), 0.000001);
+		assertEquals(PointCharge.K, charge2.fieldMagnitude(0, 0, 1), 0.000001);    
 	}   
 }
