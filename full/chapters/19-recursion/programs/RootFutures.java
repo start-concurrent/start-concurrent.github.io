bd142a45a0474f3bfbf0cc8c33f20d62f3f43b9f
Barry Wittman <bwittman@gmail.com>
2019-07-31 17:23:41 -0400

    Updated Chapter 19 up to exercises

--- full/chapters/19-recursion/programs/RootFutures.java
+++ full/chapters/19-recursion/programs/RootFutures.java
@@ -1,40 +1,34 @@
-import java.util.concurrent.*;
+import java.util.concurrent.*; //<.>
 import java.util.ArrayList;
 
 public class RootFutures {
-    private static final int THREADS = 10;
+    private static final int THREADS = 10; //<.>
     private static final int N = 100000000;
     private static final int FUTURES = 1000;
     
     public static void main(String[] args) {
-        ArrayList<Future<Double>> futures =
-            new ArrayList<Future<Double>>(FUTURES);     
-        ExecutorService executor =
-            Executors.newFixedThreadPool(THREADS);
-        int work = N / FUTURES;
+        ArrayList<Future<Double>> futures = new ArrayList<>(FUTURES);     //<.>
+        ExecutorService executor = Executors.newFixedThreadPool(THREADS); //<.>
+        int work = N/FUTURES; //<.>
         
         System.out.println("Creating futures...");
         for(int i = 0; i < FUTURES; i++) {
-            Callable<Double> summer =
-                new RootSummer(1 + i*work, 1 + (i + 1)*work);
-            Future<Double> future = executor.submit(summer);
+            Callable<Double> summer = new RootSummer(1 + i*work, 1 + (i + 1)*work); //<.>
+            Future<Double> future = executor.submit(summer); //<.>
             futures.add(future);
         }
         
         System.out.println("Getting results from futures...");
         double sum = 0;
-        for(Future<Double> future : futures) {
+        for(Future<Double> future : futures) { //<.>
             try {
-                sum += future.get();
+                sum += future.get(); //<.>
             }
-            catch(InterruptedException e) {
-                e.printStackTrace();
-            }
-            catch (ExecutionException e) {
+            catch(InterruptedException | ExecutionException e) { //<.>
                 e.printStackTrace();
             }
         }
-        executor.shutdown();
-        System.out.println("The sum of square roots is: " + sum);       
+        executor.shutdown(); //<.>
+        System.out.println("The sum of square roots is: " + sum); //<.>
     }
 }
--- full/chapters/19-recursion/programs/RootSummer.java
+++ full/chapters/19-recursion/programs/RootSummer.java
@@ -1,15 +1,15 @@
-import java.util.concurrent.*;
+import java.util.concurrent.*;				//<.>
 
 public class RootSummer implements Callable<Double> {
     private int min;
     private int max;
     
-    public RootSummer(int min, int max) {
+    public RootSummer(int min, int max) { 	//<.>
         this.min = min;
         this.max = max;
     }   
     
-    public Double call() {
+    public Double call() { 					//<.>
         double sum = 0.0;
         for(int i = min; i < max; ++i)
             sum += Math.sqrt(i);
