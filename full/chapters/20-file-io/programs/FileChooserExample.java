000faa7f24202afa3d8479c4e7e31670e57eb81e
Barry <bwittman@gmail.com>
2019-08-01 14:12:41 -0400

    Added JFileChooser section in Chapter 20

--- full/chapters/20-file-io/programs/FileChooserExample.java
+++ full/chapters/20-file-io/programs/FileChooserExample.java
@@ -1,17 +1,17 @@
-import javax.swing.*;
+import javax.swing.JFileChooser; //<.>
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 
 public class FileChooserExample {
 	public static void main(String[] args) {
-		JFileChooser chooser = new JFileChooser();
+		JFileChooser chooser = new JFileChooser();  //<.>
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("Images", "jpg", "png");
-		chooser.setFileFilter(filter);
+		chooser.setFileFilter(filter); //<.>
 		
-		int result = chooser.showOpenDialog(null);
-		if(result == JFileChooser.APPROVE_OPTION) {
+		int result = chooser.showOpenDialog(null);  //<.>
+		if(result == JFileChooser.APPROVE_OPTION) { //<.>
 			File file = chooser.getSelectedFile();
-			if(file != null && file.exists()) {
+			if(file != null && file.exists()) {		//<.>
 				long size = file.length();
 				System.out.println("The file contains " + size + " bytes.");
 			}
@@ -19,6 +19,6 @@ public class FileChooserExample {
 				System.out.println("The file doesn't exist.");
 		}
 		else
-			System.out.println("Something went wrong.");
+			System.out.println("The user probably canceled.");
 	}
 }
