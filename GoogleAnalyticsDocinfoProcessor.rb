03fc312d332a742823582b72c8b8b2fca5e1838c
Tim Korb <jtkorb@bikmort.com>
2023-06-27 18:15:05 -0400

    Update Google Analytics gstag from UA to GA4 tag

--- GoogleAnalyticsDocinfoProcessor.rb
+++ GoogleAnalyticsDocinfoProcessor.rb
@@ -13,14 +13,14 @@ Asciidoctor::Extensions.register do
       at_location :footer
       process do |doc|
         next unless (ga_account_id = doc.attr 'google-analytics-account')
-        %(<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create','#{ga_account_id}','auto');
-ga('send','pageview');
-</script>)
+        %(<script async src="https://www.googletagmanager.com/gtag/js?id=#{ga_account_id}"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '#{ga_account_id}');
+          </script>)
       end
     end
   end
